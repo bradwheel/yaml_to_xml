a2e051a2a2dc0c688f86524960f543b47001979d
Bradley Wheel <brad@policygenius.com>
2019-10-25 16:11:11 -0400

    Add formatting

--- gen.py
+++ gen.py
@@ -1,5 +1,5 @@
 import yaml
-
+import textwrap
 
 
 def open_tag(tag_name, attrs=None):
@@ -13,41 +13,51 @@ def open_tag(tag_name, attrs=None):
 def close_tag(tag_name):
     return "</{}>".format(tag_name)
 
+def add_indent(text, indent=1):
+    return textwrap.indent(text=text, prefix=" " * 2 * indent)
+
+def add_newline(text):
+    return text + "\n"
 
 
-def build_xml(data):
+def build_xml(data, indent=1):
     xml = ""
 
     if isinstance(data, dict):
-        xml += handle_dict(data)
+        xml += handle_dict(data, indent)
     else:
         xml += handle_value(data)
-    
+
     return xml
 
 
 def handle_value(data):
     return data
 
-def handle_dict(data):
+def handle_dict(data, indent):
     inner_xml = ""
 
     for (tag, value) in data.items():
 
-        node_attrs = isinstance(data[tag], dict) and data[tag].get('attrs')
+        node_attrs = isinstance(data[tag], dict) and data[tag].get('_attrs')
 
         if node_attrs:
-            node_value = data[tag].get('value', '')
+            node_value = data[tag].get('_value', '')
 
-            inner_xml += open_tag(tag, attrs=node_attrs)
-            inner_xml += node_value
+            inner_xml += add_newline(add_indent(open_tag(tag, attrs=node_attrs), indent=indent+1))
+            inner_xml += add_newline(add_indent(node_value, indent=indent+2))
+            inner_xml += add_newline(add_indent(close_tag(tag), indent=indent+1))
 
             data[tag] = None
         else:
-            inner_xml += open_tag(tag)
-            inner_xml += build_xml(value)
-
-        inner_xml += close_tag(tag)
+            if isinstance(value, dict):
+                inner_xml += add_newline(add_indent(open_tag(tag), indent=indent+1))
+                inner_xml += build_xml(value, indent=indent+1)
+                inner_xml += add_newline(add_indent(close_tag(tag), indent=indent+1))
+            else:
+                inner_xml += add_indent(open_tag(tag), indent=indent+1)
+                inner_xml += build_xml(value, indent=indent+1)
+                inner_xml += add_newline(close_tag(tag))
 
     return inner_xml
 
